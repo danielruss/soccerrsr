1d07181065684c7396403c204bd73aaa9a8dd40d
Daniel Russ <druss@mail.nih.gov>
2026-07-23 13:47:57 -0400

    clean up warnings..1

--- R/soccer.R
+++ R/soccer.R
@@ -65,14 +65,14 @@ run_soccernet <- function(df,n=10,block_size=8){
     cat("...SOCcerNET requires an Id column, since you did not provide one, I am creating one with the format row-xxx where xxx is the row number\n")
     num_chars=floor(log10(nrow(df)))+1
     df["Id"]=paste0("row-",stringr::str_pad(1:nrow(df),num_chars,"left","0") )
-    df <- df |> dplyr::relocate(Id)
+    df <- df |> dplyr::relocate("Id")
   }
   if (!"JobTask" %in% names(df)){
     cat("...You did not supply a JobTask column... filling with empty strings\n")
     df["JobTask"] = ""
   }
   xw_cols <- c("soc1980","noc2011","isco1988")
-  df1 <- df |> dplyr::mutate(dplyr::across(any_of(xw_cols),\(c){if (is.list(c)) {c} else {as.list(c)}}) )
+  df1 <- df |> dplyr::mutate(dplyr::across(dplyr::any_of(xw_cols),\(c){if (is.list(c)) {c} else {as.list(c)}}) )
   res <- soccer_net(df1,n,block_size)
   df |> dplyr::left_join(res,by="Id")
 }
